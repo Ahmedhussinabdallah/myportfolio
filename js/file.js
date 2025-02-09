bf54ca49eff1be11cff46e3158270866cf0c35e8
Ahmedhussinabdallah <140561178+Ahmedhussinabdallah@users.noreply.github.com>
2025-02-09 16:53:03 +0200

    Add files via upload

---
+++ js/file.js
@@ -0,0 +1,13 @@
+let menu = document.getElementById("my-menu");
+
+let closeShow= document.getElementById('hidden')
+let Show= document.getElementById('show')
+
+
+function toggleMenu(){
+    if(menu.style.display==="none"||menu.style.display===""){
+        menu.style.display="block";
+    }else{
+        menu.style.display="none";
+    }
+};  
